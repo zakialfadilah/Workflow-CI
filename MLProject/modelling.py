c861a734bd120f46b97ea1d201e1969ee50af68a
Zaki Alfadilah <zakialfadilah@Zakis-MacBook-Pro.local>
2025-12-15 01:09:21 +0700

    Add preprocessed dataset for CI training

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -6,9 +6,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
-# SET EXPERIMENT (BOLEH, AMAN)
-mlflow.set_experiment("Loan Prediction CI Experiment")
-
+# =========================
+# LOAD DATA (RELATIVE PATH)
+# =========================
 DATA_PATH = "LoanPrediction_preprocessing.csv"
 
 df = pd.read_csv(DATA_PATH)
@@ -20,19 +20,23 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
+# =========================
+# TRAIN MODEL
+# =========================
 model = LogisticRegression(max_iter=1000)
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
 acc = accuracy_score(y_test, y_pred)
 
-# LOG MANUAL (INI YANG DINILAI)
+# =========================
+# MANUAL LOGGING (ADVANCE)
+# =========================
 mlflow.log_param("model_type", "LogisticRegression")
 mlflow.log_metric("accuracy", acc)
 
-# REGISTER MODEL
 mlflow.sklearn.log_model(
-    sk_model=model,
+    model,
     artifact_path="model",
     registered_model_name="LoanPredictionModel"
 )
