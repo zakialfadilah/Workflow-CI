7dc2501ad98c08747e56234648037c128b4209d4
Zaki Alfadilah <zakialfadilah@Zakis-MacBook-Pro.local>
2025-12-15 01:06:35 +0700

    Add preprocessed dataset for CI training

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,16 +1,17 @@
+import pandas as pd
 import mlflow
 import mlflow.sklearn
-from sklearn.ensemble import RandomForestClassifier
+
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-import pandas as pd
 
-mlflow.set_tracking_uri(
-    "https://dagshub.com/zakialfadilah/loan-prediction-mlflow.mlflow"
-)
-mlflow.set_experiment("Loan Prediction Experiment")
+# SET EXPERIMENT (BOLEH, AMAN)
+mlflow.set_experiment("Loan Prediction CI Experiment")
 
-df = pd.read_csv("LoanPrediction_preprocessing.csv")
+DATA_PATH = "LoanPrediction_preprocessing.csv"
+
+df = pd.read_csv(DATA_PATH)
 
 X = df.drop("Loan_Status", axis=1)
 y = df["Loan_Status"]
@@ -19,15 +20,21 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-with mlflow.start_run():
-    model = RandomForestClassifier(n_estimators=100, random_state=42)
-    model.fit(X_train, y_train)
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train, y_train)
 
-    acc = accuracy_score(y_test, model.predict(X_test))
-    mlflow.log_metric("accuracy", acc)
+y_pred = model.predict(X_test)
+acc = accuracy_score(y_test, y_pred)
 
-    mlflow.sklearn.log_model(
-        model,
-        artifact_path="model",
-        registered_model_name="LoanPredictionModel"
-    )
+# LOG MANUAL (INI YANG DINILAI)
+mlflow.log_param("model_type", "LogisticRegression")
+mlflow.log_metric("accuracy", acc)
+
+# REGISTER MODEL
+mlflow.sklearn.log_model(
+    sk_model=model,
+    artifact_path="model",
+    registered_model_name="LoanPredictionModel"
+)
+
+print("Training finished. Accuracy:", acc)
